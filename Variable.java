8432150cb5902dc2791a837dddd9cef2f2615bed
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-14 17:18:26 +0900

    [add] 変数の練習

---
+++ Variable.java
@@ -0,0 +1,10 @@
+class Variable {
+  public static void main(String[] args) {
+    int month;
+    month = 9;
+    int day = 16;
+
+    System.out.println("今回のファイルは");
+    System.out.println(month + "月" + day + "日" + "に作成しました");
+  }
+}
