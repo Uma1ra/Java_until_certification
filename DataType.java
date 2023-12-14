e79a7a6e45d79518298b844e19acada6097ef68d
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-14 17:23:20 +0900

    [add] different data types and variables

---
+++ DataType.java
@@ -0,0 +1,13 @@
+class DataType {
+  public static void main(String[] args) {
+    int month = 11;
+    int day = 4;
+    float weight = 63.0f;
+    double height = 157.5;
+    char bloodType = "AB";
+
+    String name = "Bambi";
+
+    
+  }
+}
